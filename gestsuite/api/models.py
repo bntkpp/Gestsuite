52da75d3396cd972c2869a8e0f7cd5fc5e55057d
matikepp <thewild91@gmail.com>
2024-09-11 20:55:07 -0300

    chore: erro corregido en la creación de usuarios superadministradores

--- gestsuite/api/models.py
+++ gestsuite/api/models.py
@@ -3,23 +3,30 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-	def create_user(self, username, password=None):
+	def create_user(self, username, email, password=None):
+		if not email:
+			raise ValueError('El email es obligatorio.')
 		if not username:
 			raise ValueError('El nombre de usuario es obligatorio.')
 		if not password:
 			raise ValueError('La contraseña es obligatoria.')
-		user = self.model(username=username)
+		email = self.normalize_email(email)
+		user = self.model(username=username, email = email)
 		user.set_password(password)
 		user.save()
 		return user
 	
-	def create_superuser(self, username, password=None):
+	def create_superuser(self, username, email, password=None):
+		if not email:
+			raise ValueError('El email es obligatorio.')
 		if not username:
 			raise ValueError('El nombre de usuario es obligatorio.')
 		if not password:
 			raise ValueError('La contraseña es obligatoria.')
-		user = self.create_user(username, password)
+		
+		user = self.create_user(username, email, password)
 		user.is_superuser = True
+		user.is_staff = True
 		user.save()
 		return user
 
@@ -28,6 +35,8 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
 	user_id = models.AutoField(primary_key=True)
 	username = models.CharField(max_length=50, unique=True)
 	email = models.EmailField(max_length=50, unique=True)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
 	
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = ['email']
